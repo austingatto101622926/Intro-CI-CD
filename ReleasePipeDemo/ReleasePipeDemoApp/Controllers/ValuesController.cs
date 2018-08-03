a84fc596640cf452f61e067a80302e055facdba7
Austin Gatto <austin.s.c.gatto@outlook.com>
2018-08-03 11:45:08 +1000

    Default Implementation

--- ReleasePipeDemo/ReleasePipeDemoApp/Controllers/ValuesController.cs
+++ ReleasePipeDemo/ReleasePipeDemoApp/Controllers/ValuesController.cs
@@ -9,7 +9,7 @@ namespace ReleasePipeDemoApp.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
+        // GET api/values//
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
